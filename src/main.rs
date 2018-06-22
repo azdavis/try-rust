15a938c7c431cbbba12dd427735373c7713018db
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-21 21:31:35 -0700

    Get a random number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+extern crate rand;
+
 use std::io;
 use std::io::Write;
+use rand::Rng;
 
 fn main() {
+    let ans = rand::thread_rng().gen_range(1, 101);
     print!("guess the number: ");
     io::stdout().flush().unwrap();
     let mut guess = String::new();
@@ -9,4 +13,5 @@ fn main() {
         .read_line(&mut guess)
         .expect("could not read line");
     println!("you guessed {}", guess);
+    println!("the true answer is {}", ans);
 }
