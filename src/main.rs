b6b0205aa161dbe188328e762b4fbfb4e28dbec7
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-21 21:10:28 -0700

    Read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("hello.");
+    print!("guess the number: ");
+    io::stdout().flush().unwrap();
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("could not read line");
+    println!("you guessed {}", guess);
 }
