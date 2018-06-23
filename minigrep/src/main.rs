bf4ad368297d94197baa568871bf61b8565531ba
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-22 23:02:32 -0700

    Do stuff with BadArgs and and_then

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,28 @@
 use std::env;
+use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+
+fn fatal(err: Box<Error>) {
+    eprintln!("{}", err);
+    process::exit(1);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        std::process::exit(1);
-    });
-    let mut file = File::open(config.filename).expect("no such file");
+    let _ = Config::new(&args)
+        .and_then(run)
+        .unwrap_or_else(fatal);
+}
+
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut file = File::open(config.filename)?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("could not read");
+    file.read_to_string(&mut contents)?;
     println!("contents = \n{}", contents);
+    Ok(())
 }
 
 struct Config {
@@ -20,12 +31,27 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Self, &'static str> {
+    fn new(args: &[String]) -> Result<Self, Box<Error>> {
         if args.len() != 3 {
-            return Err("not enough args");
+            return Err(Box::new(BadArgs));
         }
         let query = args[1].clone();
         let filename = args[2].clone();
         Ok(Config { query, filename })
+    }
+}
+
+#[derive(Debug)]
+struct BadArgs;
+
+impl Error for BadArgs {
+    fn description(&self) -> &str {
+        "bad args"
+    }
+}
+
+impl fmt::Display for BadArgs {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.description())
     }
 }
