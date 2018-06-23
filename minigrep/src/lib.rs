5c4534e97e7158126571c3455eb6b5f9c5efe02e
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-23 01:19:01 -0700

    Use search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,7 +8,9 @@ pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
     let mut file = File::open(config.filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    println!("contents = \n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
 }
 
