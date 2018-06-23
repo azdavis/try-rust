3ec5a73800511ade29bf773c2d5882ac5dde4dfd
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-22 20:40:29 -0700

    Call it query

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,9 +3,9 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
-        eprintln!("usage: {} <regex> <file>", args[0]);
+        eprintln!("usage: {} <query> <file>", args[0]);
         std::process::exit(1);
     }
-    let regex = &args[1];
+    let query = &args[1];
     let file = &args[2];
 }
