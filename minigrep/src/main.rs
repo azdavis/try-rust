3749c182fda5d86ea5a5804f2ba3be4823b4459b
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-22 20:40:23 -0700

    Use a use

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,7 @@
+use std::env;
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
         eprintln!("usage: {} <regex> <file>", args[0]);
         std::process::exit(1);
