61b48df567d939c336f5038b74b53cafaa56e126
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-22 22:24:40 -0700

    Refactor into Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,14 +4,28 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("usage: {} <query> <file>", args[0]);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
         std::process::exit(1);
-    }
-    let query = &args[1];
-    let file = &args[2];
-    let mut file = File::open(file).expect("no such file");
+    });
+    let mut file = File::open(config.filename).expect("no such file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("could not read");
     println!("contents = \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() != 3 {
+            return Err("not enough args");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
