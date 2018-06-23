24a1bad4e45bc9f02bd4ba1cbc51c2d6ed27c2cf
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-22 20:36:37 -0700

    Get regex and file from args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("args = {:?}", args);
+    if args.len() != 3 {
+        eprintln!("usage: {} <regex> <file>", args[0]);
+        std::process::exit(1);
+    }
+    let regex = &args[1];
+    let file = &args[2];
+    println!("regex = {}, file = {}", regex, file);
 }
