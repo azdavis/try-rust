4f57d2f847dfaed5c76ccd3b6adbb9256765b736
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-26 20:54:25 -0700

    Match stream

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -3,7 +3,9 @@ use std::net::TcpListener;
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connection OK.");
+        match stream {
+            Ok(x) => println!("ok: {:?}", x),
+            Err(e) => eprintln!("err: {}", e),
+        };
     }
 }
