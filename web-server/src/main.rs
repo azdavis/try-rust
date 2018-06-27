a0dc8af6dd65d96f7b242cd5c95adea5a9553fa7
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-26 20:57:56 -0700

    Use handle_conn

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,11 +1,19 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(x) => println!("ok: {:?}", x),
+            Ok(x) => handle_conn(x),
             Err(e) => eprintln!("err: {}", e),
         };
     }
+}
+
+fn handle_conn(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("request: {}", String::from_utf8_lossy(&buffer[..]));
 }
