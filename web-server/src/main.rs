4a62c609742d2e25b2308965a2fa99fb336b7082
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-26 20:49:54 -0700

    Listen to connections

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection OK.");
+    }
 }
