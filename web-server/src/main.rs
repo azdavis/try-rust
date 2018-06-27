5fc61685ed3f141631eebb141565e7a89a95b7ac
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-26 21:14:43 -0700

    Respond with some HTML

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -2,6 +2,13 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+const RESPONSE: &'static str = concat!(
+    "HTTP/1.1 200 OK",
+    "\r\n",
+    "\r\n",
+    include_str!("../static/index.html"),
+);
+
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
     for stream in listener.incoming() {
@@ -16,4 +23,6 @@ fn handle_conn(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+    stream.write(RESPONSE.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
