187256ebd69be980e33dc06865babd426b1c67cf
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-29 20:49:30 -0700

    Use a let
    aka more BS with no reftrans

--- web-server/src/thread_pool/worker.rs
+++ web-server/src/thread_pool/worker.rs
@@ -15,7 +15,8 @@ impl Worker {
     pub fn new(id: usize, rx: Arc<Mutex<mpsc::Receiver<Msg>>>) -> Self {
         let handle = Some(thread::spawn(move || {
             loop {
-                match rx.lock().unwrap().recv().unwrap() {
+                let msg = rx.lock().unwrap().recv().unwrap();
+                match msg {
                     Msg::Exec(f) => {
                         println!("id {} got a Exec", id);
                         f.call_box();
