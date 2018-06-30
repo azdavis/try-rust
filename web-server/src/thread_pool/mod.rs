78e260eec1d847f154a4b24c3e801e24b40fa7db
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-29 19:44:30 -0700

    Start working with channels and Job

--- web-server/src/thread_pool/mod.rs
+++ web-server/src/thread_pool/mod.rs
@@ -1,8 +1,13 @@
+use std::sync::mpsc;
+
 mod worker;
 
 use self::worker::Worker;
 
+pub struct Job;
+
 pub struct ThreadPool {
+    tx: mpsc::Sender<Job>,
     workers: Vec<Worker>,
 }
 
@@ -10,11 +15,12 @@ impl ThreadPool {
     /// Create a new ThreadPool with size many workers. Panic if size == 0.
     pub fn new(size: usize) -> Self {
         assert!(size != 0);
+        let (tx, rx) = mpsc::channel();
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, rx));
         }
-        ThreadPool { workers }
+        ThreadPool { tx, workers }
     }
 
     /// Register f for later execution. As soon as at least one of the threads
--- web-server/src/thread_pool/worker.rs
+++ web-server/src/thread_pool/worker.rs
@@ -1,14 +1,18 @@
+use std::sync::mpsc;
 use std::thread;
+
+use super::Job;
 
 pub struct Worker {
     id: usize,
+    rx: mpsc::Receiver<Job>,
     handle: thread::JoinHandle<()>,
 }
 
 impl Worker {
     /// Create a new worker.
-    pub fn new(id: usize) -> Self {
+    pub fn new(id: usize, rx: mpsc::Receiver<Job>) -> Self {
         let handle = thread::spawn(|| ());
-        Worker { id, handle }
+        Worker { id, rx, handle }
     }
 }
