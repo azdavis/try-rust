05fef16b0c5ece0ee2e89d3e68c499561903e2ae
Ariel Davis <ariel.z.davis@icloud.com>
2018-06-29 20:05:10 -0700

    Work on handle

--- web-server/src/thread_pool/worker.rs
+++ web-server/src/thread_pool/worker.rs
@@ -7,14 +7,16 @@ use super::Job;
 
 pub struct Worker {
     id: usize,
-    rx: Arc<Mutex<mpsc::Receiver<Job>>>,
     handle: thread::JoinHandle<()>,
 }
 
 impl Worker {
     /// Create a new worker.
     pub fn new(id: usize, rx: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
-        let handle = thread::spawn(|| ());
-        Worker { id, rx, handle }
+        let handle = thread::spawn(move || {
+            let f = rx.lock().unwrap().recv().unwrap();
+            f();
+        });
+        Worker { id, handle }
     }
 }
