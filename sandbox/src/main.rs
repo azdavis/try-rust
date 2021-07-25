087c5ff16e2551b255983b71650b3f71675ac9c0
Ariel Davis <ariel.z.davis@icloud.com>
2021-07-25 09:23:54 -0700

    Compile all modules

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+mod eat;
+mod empty_let;
+mod from_fn;
+mod if_let;
+mod iter;
+mod lazy;
+mod multi_mut_borrow;
+mod mut_ref;
+mod partial_move;
+mod rect;
+mod slice;
 mod trait_obj;
 
 fn main() {
